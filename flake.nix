4ffb4aa543979bfc4c6d8ba7382e7120dd2a2b36
himkt <himkt@klis.tsukuba.ac.jp>
2024-12-04 19:02:17 +0900

    misc: template names

--- flake.nix
+++ flake.nix
@@ -6,17 +6,9 @@
   outputs = { self, nixpkgs, ... }:
     {
       templates = {
-        standalone = {
-          path = ./templates/standalone;
-          description = "Standalone setup";
-        };
-        nixos = {
-          path = ./templates/nixos;
-          description = "Home Manager as a NixOS module,";
-        };
-        nix-darwin = {
-          path = ./templates/nix-darwin;
-          description = "Home Manager as a nix-darwin module,";
+        devenv = {
+          path = ./templates/nix-devenv;
+          description = "devenv template with uv";
         };
       };
     }
